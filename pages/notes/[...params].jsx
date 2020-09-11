1f4c9144045ba242846108034ee1d0b045db56c1
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-09-11 09:15:50 -0700

    Routes using a catch-all route.

--- pages/notes/[...params].jsx
+++ pages/notes/[...params].jsx
@@ -3,9 +3,10 @@ import { useRouter, withRouter } from "next/router";
 
 const Page = () => {
   const router = useRouter();
-  // id is the name of the file
+  // params is the array of dynamic route components
+  // eg: /notes/1/2/3 => params = [1, 2, 3] see the console log result for vuewing this result.
   const { params } = router.query;
-
+  console.log(params);
   return <h1>Note: {params}</h1>;
 };
 
