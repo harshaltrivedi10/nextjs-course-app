220c04b3f867538cd9cd2f4421003bcd20527971
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-09-11 09:23:46 -0700

    setup with src folder.

--- src/pages/notes/[...params].jsx
+++ src/pages/notes/[...params].jsx
@@ -4,7 +4,7 @@ import { useRouter, withRouter } from "next/router";
 const Page = () => {
   const router = useRouter();
   // params is the array of dynamic route components
-  // eg: /notes/1/2/3 => params = [1, 2, 3] see the console log result for vuewing this result.
+  // eg: /notes/1/2/3 => params = [1, 2, 3] see the console log result for
   const { params } = router.query;
   console.log(params);
   return <h1>Note: {params}</h1>;
